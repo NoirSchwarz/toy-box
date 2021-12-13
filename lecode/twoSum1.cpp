d2fed4223183ecff7b3b34f635d3c6c3dc6686dc
NoirSchwarz <1030604862@qq.com>
2021-12-14 00:43:02 +0800

    ”update”

--- lecode/twoSum1.cpp
+++ lecode/twoSum1.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
+#include <unordered_map>
 #include <vector>
 using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target)
+    {
+        unordered_map<int, int> hashtable;
+        for (int i = 0; i < nums.size(); i++) {
+            auto it = hashtable.find(target - nums[i]);
+            if (it != hashtable.end()) {
+                return { it->second, i };
+            }
+
+            hashtable[nums[i]] = i;
+        }
+        return {};
+    }
+};
 
 int main()
 {
