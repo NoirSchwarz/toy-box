cd01f9ab8306ec6ac1b2007dee6c5aca483dcf10
NoirSchwarz <1030604862@qq.com>
2021-12-15 02:07:09 +0800

    ”update”

--- lecode/3_lengthOfLongetSubstring.cpp
+++ lecode/3_lengthOfLongetSubstring.cpp
@@ -4,11 +4,24 @@ using namespace std;
 
 class Solution {
 public:
-    int lengthOfLongestSubstring(sting s)
+    int lengthOfLongestSubstring(string s)
     {
         unordered_set<char> occ;
         int n = s.size();
         int right_point = -1;
-        for ()
+        int length = 0;
+        for (int i = 0; i < n; i++) {
+            if (i != 0) {
+                occ.erase(s[i - 1]);
+            }
+
+            while (right_point + 1 < n && occ.count(s[right_point + 1])) {
+                occ.insert(s[right_point + 1]);
+                ++right_point;
+            }
+
+            length = max(length, right_point + 1 - i);
+        }
+        return length;
     }
-}
+};
